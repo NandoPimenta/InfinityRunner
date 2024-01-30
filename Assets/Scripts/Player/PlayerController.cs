1b42902a48635ce77af8911247759fb0ad7361a7
Fernando H. Pimenta <fernandopimentadev@gmail.com>
2024-01-30 19:57:17 -0300

    finished gamemobile1

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,70 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Lerp")]
+    public Transform target;
+    public float learpSpeed = 1f;
 
     public float speed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string tagToCheckEnemy = "Enemy";
+    public string tagToCheckEndline = "EndLine";
 
-    // Update is called once per frame
+    public GameObject startScreen;
+    public GameObject endScreen;
+
+    //private
+    private bool _canRun = false;
+    private Vector3 _pos;
+
+
+
+
+
+
     void Update()
     {
+        if (!_canRun) { return; }
+
+        _pos = target.position;
+
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+  
+       
+
+
+        transform.position = Vector3.Lerp(transform.position, _pos, learpSpeed * Time.deltaTime);
+
+
         transform.Translate(transform.forward * speed *Time.deltaTime);
     }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        if(other.transform.tag == tagToCheckEndline)
+        {
+            _canRun = false;
+            endScreen.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+            endScreen.SetActive(true);
+        }
+    }
+
+
+    public void StartToRun()
+    {
+        _canRun = true;
+        startScreen.SetActive(false);
+    }
+
 }
